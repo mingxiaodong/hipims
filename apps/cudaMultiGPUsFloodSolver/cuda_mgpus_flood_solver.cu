518e2f9c7964edf78ceb7842d27201def0ff395f
Xiaodong Ming <xiaodong.ming@outlook.com>
2020-06-10 11:25:13 +0100

    Update cuda_mgpus_flood_solver.cu
    sychronize problem in docker barrier.wait();

--- apps/cudaMultiGPUsFloodSolver/cuda_mgpus_flood_solver.cu
+++ apps/cudaMultiGPUsFloodSolver/cuda_mgpus_flood_solver.cu
@@ -366,7 +366,8 @@ void run(cuDataBank& bank, std::vector<int> device_list, unsigned int domain_id,
     }
 
   } while (!time_controller.is_end());
-
+  // sychronize problem in docker
+  barrier.wait();
   printf("Writing maximum inundated depth of domain %d.\n", domain_id);
   raster_writer.write(h_max, "h_max", t_all, output_directory.c_str());
 
